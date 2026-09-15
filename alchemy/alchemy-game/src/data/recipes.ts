a054d422a6a22adb2e19ede32232feb78769c6c4
Mrak0ss <tom2003pl@gmail.com>
2026-09-15 10:09:34 +0200

    Update recipes.ts

--- alchemy/alchemy-game/src/data/recipes.ts
+++ alchemy/alchemy-game/src/data/recipes.ts
@@ -2826,7 +2826,7 @@ export const recipes: Record<string, RecipeOutput> = {
 "skin+thorn": "sting",
 "life+pressure": "stress",
 "heat+skin": "sweat",
-"coin+head": "tail",
+"coin+head": "tails",
 "drop+salt": "tear",
 "feather+skin": "tickle",
 "dentist+tool": "tooth",
